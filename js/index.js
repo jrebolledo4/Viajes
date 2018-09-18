3bfb1c138e3a5b8548517251cb083f02bcbb2fcc
JFuentes4 <otro64@hotmail.com>
2018-09-18 14:27:18 -0500

    se corrigio problema en barra de menu y en texto desplazable, ademas se comenzo la creacion de la pagina ventas

--- js/index.js
+++ js/index.js
@@ -2,16 +2,18 @@ window.onscroll = function() {
 	console.log(scrollY);
     if(window.scrollY > 700 && window.scrollY < 1000) {
       document.getElementById('animationOfContainer').className = 'contenedorAnimate';
-    } 
+		} 
   	else if(window.scrollY > 0) {
-  	document.getElementById('navbar').className = 'barra__black';
+		document.getElementById('navbar').className = 'barra__black';
+		document.getElementById('items-wrapper').className = 'menu alter'
   	}
   	else if(window.scrollY == 0) {
-  		document.getElementById('navbar').className = 'navbar2';
-  	} 
-  	else if(window.scrollY > 700 && window.scrollY < 1000) {
-  		document.getElementById('animationOfContainer').className = 'contenedorAnimate';
-  	} 
+			document.getElementById('navbar').className = 'navbar2';
+			document.getElementById('items-wrapper').className = 'menu';
+		} 
+		if(window.scrollY < 700) {
+      document.getElementById('animationOfContainer').className = 'contenedor2';
+    } 
 }
 
 	
